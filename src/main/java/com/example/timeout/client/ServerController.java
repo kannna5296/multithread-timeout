a9c477bf5446322f5bdb1f9b03e43af807236a71
kannna5296 <kannnahacks5296@gmail.com>
2021-07-18 18:10:24 +0900

    ソース追加

---
+++ src/main/java/com/example/timeout/client/ServerController.java
@@ -0,0 +1,28 @@
+package com.example.timeout.client;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+
+	@GetMapping("/")
+	public ResponseEntity<User> index(){
+		User user = new User(1, "John");
+		ResponseEntity<User> res = new ResponseEntity<User>(user, HttpStatus.valueOf(200));
+
+		try {
+			//呼び出し元でタイムアウトさせるために敢えて5秒遅くする
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			//e.printStackTrace();
+			return res;
+		}
+
+		return res;
+	}
+
+
+}
